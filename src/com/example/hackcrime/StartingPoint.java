947899d20b4a238a0ba0d689b6570985ba982344
lvwt2d <lvwt2d@mail.umsl.edu>
2014-07-22 17:48:03 -0500

    hack work
    Started the proj.  on a Saturday at work

--- src/com/example/hackcrime/StartingPoint.java
+++ src/com/example/hackcrime/StartingPoint.java
@@ -1,20 +1,28 @@
 package com.example.hackcrime;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class StartingPoint extends Activity {
+public class StartingPoint extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
 		setContentView(R.layout.activity_starting_point);
+	    GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		//I need to save that
 		getMenuInflater().inflate(R.menu.starting_point, menu);
 		return true;
 	}
